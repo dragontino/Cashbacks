62b7995cfd02ea1d9178eb7829b33415c7a2bdca
name1980 <petrovsd2002@gmail.com>
2024-01-15 03:10:28 +0300

    Initial commit

--- app/src/main/kotlin/com/cashbacks/app/util/Extensions.kt
+++
@@ -1,6 +0,0 @@
-package com.cashbacks.app.util
-
-import android.os.Bundle
-
-internal fun Bundle?.getString(key: String, defaultValue: String = "") =
-    this?.getString(key)?.takeIf { it != "null" } ?: defaultValue
