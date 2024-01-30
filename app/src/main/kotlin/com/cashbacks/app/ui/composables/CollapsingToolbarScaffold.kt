314cc45f6c84278fa9bf1849473ccfb8b20c3cd9
name1980 <petrovsd2002@gmail.com>
2024-01-31 02:08:35 +0300

    Обновлено положение floating action buttons: теперь можно добавлять множество кнопок, они будут располагаться в правом нижнем углу экрана

--- app/src/main/kotlin/com/cashbacks/app/ui/composables/CollapsingToolbarScaffold.kt
+++ app/src/main/kotlin/com/cashbacks/app/ui/composables/CollapsingToolbarScaffold.kt
@@ -1,22 +1,30 @@
 package com.cashbacks.app.ui.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.tappableElement
 import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.unit.dp
 import com.cashbacks.app.util.animate
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,18 +35,21 @@ fun CollapsingToolbarScaffold(
     topBarState: TopAppBarState = rememberTopAppBarState(
         initialContentOffset = 100f
     ),
-    floatingActionButton: @Composable (() -> Unit) = {},
-    fabPosition: FabPosition = FabPosition.Center,
+    floatingActionButtons: @Composable (ColumnScope.() -> Unit) = {},
     snackbarHost: @Composable (() -> Unit) = {},
     contentWindowInsets: WindowInsets = CollapsingToolbarScaffoldDefaults.contentWindowInsets(),
     content: @Composable ((PaddingValues) -> Unit),
 ) {
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+
+        }
+    }
+
 
     Scaffold(
         modifier = modifier,
         topBar = topBar,
-        floatingActionButton = floatingActionButton,
-        floatingActionButtonPosition = fabPosition,
         snackbarHost = snackbarHost,
         contentWindowInsets = contentWindowInsets,
         containerColor = MaterialTheme.colorScheme.background.animate(),
@@ -52,6 +63,16 @@ fun CollapsingToolbarScaffold(
             ) {
                 topBar()
             }*/
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.End,
+                modifier = Modifier
+                    .windowInsetsPadding(WindowInsets.tappableElement)
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd),
+                content = floatingActionButtons
+            )
         }
     }
 }
