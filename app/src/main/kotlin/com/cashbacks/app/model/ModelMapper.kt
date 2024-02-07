d006e9e2ecaca62d94f43ce4ec698b3edede2e07
name1980 <petrovsd2002@gmail.com>
2024-02-07 12:18:28 +0300

    Обновлена отрисовка логотипа платежной системы

--- app/src/main/kotlin/com/cashbacks/app/model/ModelMapper.kt
+++ app/src/main/kotlin/com/cashbacks/app/model/ModelMapper.kt
@@ -16,11 +16,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.cashbacks.app.R
+import com.cashbacks.domain.model.AppException
 import com.cashbacks.domain.model.ColorDesign
+import com.cashbacks.domain.model.EntryAlreadyExistsException
+import com.cashbacks.domain.model.InsertionException
 import com.cashbacks.domain.model.PaymentSystem
+import com.cashbacks.domain.model.SettingsNotFoundException
 
 data object ColorDesignMapper {
     fun ColorDesign.title(context: Context): String = when (this) {
@@ -38,11 +44,19 @@ data object ColorDesignMapper {
 
 
 data object PaymentSystemMapper {
+    val PaymentSystem?.title @Composable
+    get(): String {
+        if (this == null) return stringResource(R.string.value_not_selected)
+        val titles = stringArrayResource(R.array.payment_systems)
+        return PaymentSystem.entries.indexOf(this).let { titles[it] }
+    }
+
     @Composable
     fun PaymentSystemImage(
         paymentSystem: PaymentSystem,
         modifier: Modifier = Modifier,
-        maxWidth: Dp = 50.dp
+        maxWidth: Dp = 50.dp,
+        drawBackground: Boolean = true
     ) {
         val painterRes = when (paymentSystem) {
             PaymentSystem.Visa -> R.drawable.visa_logo
@@ -53,6 +67,14 @@ data object PaymentSystemMapper {
             PaymentSystem.AmericanExpress -> R.drawable.american_express_logo
         }
 
+        val backgroundModifier = when {
+            drawBackground -> Modifier
+                .clip(MaterialTheme.shapes.extraSmall)
+                .background(Color.White)
+                .padding(5.dp)
+            else -> Modifier
+        }
+
         Image(
             painter = painterResource(painterRes),
             contentDescription = "payment system image",
@@ -60,9 +82,33 @@ data object PaymentSystemMapper {
             modifier = Modifier
                 .then(modifier)
                 .widthIn(max = maxWidth)
-                .clip(MaterialTheme.shapes.extraSmall)
-                .background(Color.White)
-                .padding(5.dp)
+                .then(backgroundModifier)
         )
+    }
+}
+
+
+data object BankCardMapper {
+    fun addSpacesToCardNumber(cardNumber: String) = buildString {
+        for (i in cardNumber.indices) {
+            if (i > 0 && i % 4 == 0) append(" ")
+            append(cardNumber[i])
+        }
+    }.trimIndent()
+
+    fun removeSpacesFromNumber(cardNumber: String) = cardNumber.filter { it != ' ' }
+}
+
+
+data object ExceptionMapper {
+    fun AppException.getMessage(context: Context) = when (this) {
+        EntryAlreadyExistsException -> context.getString(R.string.entry_already_exists_exception)
+        is InsertionException -> this.message ?: ""
+        SettingsNotFoundException -> context.getString(R.string.settings_not_found_exception)
+    }
+
+    fun Throwable.getMessage(context: Context) = when (this) {
+        is AppException -> getMessage(context)
+        else -> ""
     }
 }
