53d64577a6165c5d01bfc368fe84b37575990fc3
name1980 <petrovsd2002@gmail.com>
2024-02-19 04:20:10 +0300

    Добавлено диалоговое окно выбора даты в календаре (для экрана редактирования кешбэка)

--- app/src/main/kotlin/com/cashbacks/app/ui/managment/DialogType.kt
+++ app/src/main/kotlin/com/cashbacks/app/ui/managment/DialogType.kt
@@ -1,7 +1,11 @@
 package com.cashbacks.app.ui.managment
 
+import java.time.LocalDate
+
 sealed interface DialogType {
     data class ConfirmDeletion<T : Any>(val value: T) : DialogType
 
     data object Save : DialogType
+
+    data class DatePicker(val date: LocalDate?) : DialogType
 }
--- app/src/main/kotlin/com/cashbacks/app/ui/managment/ScreenEvents.kt
+++ app/src/main/kotlin/com/cashbacks/app/ui/managment/ScreenEvents.kt
@@ -4,5 +4,5 @@ sealed interface ScreenEvents {
     data class OpenDialog(val type: DialogType) : ScreenEvents
     data object CloseDialog : ScreenEvents
     data class ShowSnackbar(val message: String) : ScreenEvents
-    data class Navigate(val route: String?) : ScreenEvents
+    data class Navigate(val args: Any?) : ScreenEvents
 }
