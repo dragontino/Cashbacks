6ac81acaba51059bc9245b3836313f11dcbb5000
name1980 <petrovsd2002@gmail.com>
2024-04-29 11:51:38 +0300

    Обновление версий приложения и некоторых библиотек

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     id("com.google.devtools.ksp")
 }
 
-val versionName by extra("1.8.2")
-val versionDate by extra("28/04/2024")
-val debugVersionExt by extra("beta25")
+val versionName by extra("1.8.3")
+val versionDate by extra("29/04/2024")
+val debugVersionExt by extra("beta26")
 
 android {
     namespace = "com.cashbacks.app"
@@ -71,12 +71,12 @@ dependencies {
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     //Compose
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.compose.ui:ui")
@@ -97,10 +97,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.6")
 
     //DI
     implementation("com.google.dagger:dagger:2.50")
