ca8cd3d634d79acf3c37c336ae8357241cd502e0
name1980 <petrovsd2002@gmail.com>
2025-09-01 18:10:41 +0300

    Shop list item is now determined by cashback id instead of position in list

--- features/home/impl/src/main/kotlin/com/cashbacks/features/home/impl/mvi/ShopsMviStore.kt
+++ features/home/impl/src/main/kotlin/com/cashbacks/features/home/impl/mvi/ShopsMviStore.kt
@@ -10,10 +10,12 @@ import com.cashbacks.features.home.impl.composables.HomeTopAppBarState
 import com.cashbacks.features.shop.domain.model.CategoryShop
 import com.cashbacks.features.shop.domain.model.Shop
 import com.cashbacks.features.shop.presentation.api.ShopArgs
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.serialization.Serializable
 
 internal sealed interface ShopsAction {
-    data class LoadShops(val shops: Map<CategoryShop, Set<Cashback>>?) : ShopsAction
+    data class LoadShops(val shops: List<ShopWithCashback>?) : ShopsAction
 }
 
 
@@ -45,8 +47,12 @@ internal sealed interface ShopsIntent {
     data object CloseDialog : ShopsIntent
     data class ChangeAppBarState(val state: HomeTopAppBarState) : ShopsIntent
 
-    data class SwipeShop(val position: Int? = null) : ShopsIntent {
-        constructor(position: Int, isSwiped: Boolean) : this(position.takeIf { isSwiped })
+    data class SwipeShop(val id: String? = null) : ShopsIntent {
+        constructor(id: String, isSwiped: Boolean) : this(id.takeIf { isSwiped })
+    }
+
+    data class SelectShop(val id: String? = null) : ShopsIntent {
+        constructor(id: String, isSelected: Boolean) : this(id.takeIf { isSelected })
     }
 }
 
@@ -55,8 +61,10 @@ internal sealed interface ShopsMessage {
     data class UpdateScreenState(val state: ScreenState) : ShopsMessage
     data class UpdateViewModelState(val state: ViewModelState) : ShopsMessage
     data class UpdateAppBarState(val state: HomeTopAppBarState) : ShopsMessage
-    data class UpdateShops(val shops: Map<CategoryShop, Set<Cashback>>?) : ShopsMessage
-    data class UpdateSelectedShopIndex(val index: Int?) : ShopsMessage
+    data class UpdateShops(val shops: ImmutableList<ShopWithCashback>?) : ShopsMessage
+
+    data class UpdateSwipedShopId(val id: String?) : ShopsMessage
+    data class UpdateSelectedShopId(val id: String?) : ShopsMessage
 }
 
 
@@ -66,6 +74,17 @@ internal data class ShopsState(
     val screenState: ScreenState = ScreenState.Stable,
     val viewModelState: ViewModelState = ViewModelState.Viewing,
     val appBarState: HomeTopAppBarState = HomeTopAppBarState.TopBar,
-    val shops: Map<CategoryShop, Set<Cashback>>? = emptyMap(),
-    val selectedShopIndex: Int? = null
+    val shops: ImmutableList<ShopWithCashback>? = persistentListOf(),
+    val swipedShopId: String? = null,
+    val selectedShopId: String? = null
 )
+
+
+@Serializable
+@Immutable
+internal data class ShopWithCashback(val shop: CategoryShop, val maxCashback: Cashback?) {
+    val id: String get() = when(maxCashback) {
+        null -> shop.id.toString()
+        else -> "${shop.id}-${maxCashback.id}"
+    }
+}
