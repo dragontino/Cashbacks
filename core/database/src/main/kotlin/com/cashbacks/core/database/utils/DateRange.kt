eac928e2395301fb0bd838ecabd05fabef50cb15
name1980 <petrovsd2002@gmail.com>
2025-09-01 15:53:45 +0300

    Replace Clock interface to LocalDate.now() extension function and ClosedRange<LocalDate> to LocalDateRange

--- core/database/src/main/kotlin/com/cashbacks/core/database/utils/DateRange.kt
+++ core/database/src/main/kotlin/com/cashbacks/core/database/utils/DateRange.kt
@@ -1,14 +1,14 @@
 package com.cashbacks.core.database.utils
 
 import com.cashbacks.common.utils.DateUtils
-import com.cashbacks.common.utils.today
+import com.cashbacks.common.utils.now
 import com.cashbacks.core.database.entity.BasicCashbackEntity
-import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateRange
 
 
-fun BasicCashbackEntity.getDateRange(): ClosedRange<LocalDate> {
-    val startDate = startDate ?: Clock.System.today()
+fun BasicCashbackEntity.getDateRange(): LocalDateRange {
+    val startDate = startDate ?: LocalDate.now()
     val endDate = expirationDate ?: DateUtils.MaxDate
     return startDate..endDate
 }
