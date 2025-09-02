40f7bc64de1da62a6e68448d93f37fa68d55cacf
name1980 <petrovsd2002@gmail.com>
2025-09-02 19:07:16 +0300

    Added posting notification in DeleteExpiredCashbacksWorker when the work is done.

--- app/src/main/kotlin/com/cashbacks/app/workers/DeleteExpiredCashbacksWorker.kt
+++ app/src/main/kotlin/com/cashbacks/app/workers/DeleteExpiredCashbacksWorker.kt
@@ -1,9 +1,23 @@
 package com.cashbacks.app.workers
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
+import com.cashbacks.app.ui.MainActivity
+import com.cashbacks.common.composables.usePermissions
 import com.cashbacks.common.resources.R
 import com.cashbacks.common.utils.now
 import com.cashbacks.features.cashback.domain.usecase.DeleteCashbacksUseCase
@@ -20,6 +34,8 @@ class DeleteExpiredCashbacksWorker(
 ) : CoroutineWorker(context, workerParams), KoinComponent {
     private companion object {
         const val TAG = "DeleteExpiredCashbacksWorker"
+
+        const val NOTIFICATION_CHANNEL_ID = "CashbacksNotificationChannel"
     }
 
     private val getExpiredCashbacks by inject<GetExpiredCashbacksUseCase>()
@@ -27,11 +43,7 @@ class DeleteExpiredCashbacksWorker(
     private val getSettings by inject<GetSettingsUseCase>()
 
 
-    private fun createNotification(message: String) {
-        Log.i(TAG, message)
-    }
-
-
+    @SuppressLint("MissingPermission")
     override suspend fun doWork(): Result {
         Log.i(TAG, "Work started")
         return try {
@@ -52,7 +64,19 @@ class DeleteExpiredCashbacksWorker(
                             count,
                             count
                         )
-                        createNotification(msg)
+                        val notification = createNotification(msg)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            usePermissions(
+                                Manifest.permission.POST_NOTIFICATIONS,
+                                context = applicationContext,
+                                grantPermission = {}
+                            ) {
+                                pushNotification(notification)
+                            }
+                        } else {
+                            pushNotification(notification)
+                        }
+                        Log.i(TAG, msg)
                     } else {
                         Log.i(TAG, "There are 0 cashbacks to delete.")
                     }
@@ -69,5 +93,54 @@ class DeleteExpiredCashbacksWorker(
         } finally {
             Log.i(TAG, "Work finished")
         }
+    }
+
+
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        return ForegroundInfo(0, createNotification("Empty notification"))
+    }
+
+
+    private fun getOrCreateNotificationChannel(): NotificationChannel {
+        val notificationManager = getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        )
+        val channel = notificationManager?.getNotificationChannel(NOTIFICATION_CHANNEL_ID)
+            ?: NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                "ExpiredCashbacksWorker",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        return channel
+    }
+
+
+
+    private fun createNotification(message: String): Notification {
+        val channel = getOrCreateNotificationChannel()
+
+        val mainActivityIntent = Intent(applicationContext, MainActivity::class.java)
+        val pendingIntentFlag = PendingIntent.FLAG_IMMUTABLE
+        val mainActivityPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            mainActivityIntent,
+            pendingIntentFlag
+        )
+        return Notification.Builder(applicationContext, channel.id)
+            .setSmallIcon(com.cashbacks.app.R.drawable.icon)
+            .setContentTitle(applicationContext.getString(com.cashbacks.app.R.string.app_name))
+            .setContentText(message)
+            .setContentIntent(mainActivityPendingIntent)
+            .setAutoCancel(true)
+            .build()
+    }
+
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    private fun pushNotification(notification: Notification) {
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        notificationManager.notify(0, notification)
     }
 }
