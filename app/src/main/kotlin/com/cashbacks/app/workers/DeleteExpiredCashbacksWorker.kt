b49615b50c3510ebf6401f088814cae625983590
Sergio Petrov <89968445+dragontino@users.noreply.github.com>
2025-09-15 16:04:40 +0300

    Updated notification channel name and notification ui. Every notification now has different ids, generated from version update date and now date.

--- app/src/main/kotlin/com/cashbacks/app/workers/DeleteExpiredCashbacksWorker.kt
+++ app/src/main/kotlin/com/cashbacks/app/workers/DeleteExpiredCashbacksWorker.kt
@@ -8,23 +8,26 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
+import com.cashbacks.app.BuildConfig
+import com.cashbacks.app.R
 import com.cashbacks.app.ui.MainActivity
 import com.cashbacks.common.composables.usePermissions
-import com.cashbacks.common.resources.R
 import com.cashbacks.common.utils.now
+import com.cashbacks.common.utils.parseToDate
 import com.cashbacks.features.cashback.domain.usecase.DeleteCashbacksUseCase
 import com.cashbacks.features.cashback.domain.usecase.GetExpiredCashbacksUseCase
 import com.cashbacks.features.settings.domain.model.Settings
 import com.cashbacks.features.settings.domain.usecase.GetSettingsUseCase
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.minus
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -34,8 +37,6 @@ class DeleteExpiredCashbacksWorker(
 ) : CoroutineWorker(context, workerParams), KoinComponent {
     private companion object {
         const val TAG = "DeleteExpiredCashbacksWorker"
-
-        const val NOTIFICATION_CHANNEL_ID = "CashbacksNotificationChannel"
     }
 
     private val getExpiredCashbacks by inject<GetExpiredCashbacksUseCase>()
@@ -60,21 +61,22 @@ class DeleteExpiredCashbacksWorker(
                 val deletionResult = deleteCashbacks(expiredCashbacks).onSuccess { count ->
                     if (count > 0) {
                         val msg = applicationContext.resources.getQuantityString(
-                            R.plurals.expired_cashbacks_deletion_success,
+                            com.cashbacks.common.resources.R.plurals.expired_cashbacks_deletion_success,
                             count,
                             count
                         )
                         val notification = createNotification(msg)
+                        val id = generateIdFromDate()
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                             usePermissions(
                                 Manifest.permission.POST_NOTIFICATIONS,
                                 context = applicationContext,
                                 grantPermission = {}
                             ) {
-                                pushNotification(notification)
+                                pushNotification(id, notification)
                             }
                         } else {
-                            pushNotification(notification)
+                            pushNotification(id, notification)
                         }
                         Log.i(TAG, msg)
                     } else {
@@ -102,17 +104,29 @@ class DeleteExpiredCashbacksWorker(
 
 
     private fun getOrCreateNotificationChannel(): NotificationChannel {
-        val notificationManager = getSystemService(
-            applicationContext,
-            NotificationManager::class.java
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+
+        val channelId = applicationContext
+            .getString(R.string.default_notification_channel_id)
+        val channel = notificationManager.getNotificationChannel(channelId)
+        if (channel != null) {
+            return channel
+        }
+
+        val newChannel = NotificationChannel(
+            channelId,
+            applicationContext.getString(R.string.default_notification_channel_name),
+            NotificationManager.IMPORTANCE_DEFAULT
         )
-        val channel = notificationManager?.getNotificationChannel(NOTIFICATION_CHANNEL_ID)
-            ?: NotificationChannel(
-                NOTIFICATION_CHANNEL_ID,
-                "ExpiredCashbacksWorker",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-        return channel
+        notificationManager.createNotificationChannel(newChannel)
+        return newChannel
+    }
+
+
+    private fun generateIdFromDate(date: LocalDate = LocalDate.now()): Int {
+        val appBuildDate = BuildConfig.VERSION_DATE.parseToDate()
+        val daysNumber = (date - appBuildDate).days
+        return daysNumber.coerceAtLeast(0)
     }
 
 
@@ -128,19 +142,32 @@ class DeleteExpiredCashbacksWorker(
             mainActivityIntent,
             pendingIntentFlag
         )
+
         return Notification.Builder(applicationContext, channel.id)
-            .setSmallIcon(com.cashbacks.app.R.drawable.icon)
-            .setContentTitle(applicationContext.getString(com.cashbacks.app.R.string.app_name))
+            .setSmallIcon(com.cashbacks.common.resources.R.drawable.cashback_filled)
+            .setLargeIcon(Icon.createWithResource(applicationContext, R.drawable.icon))
+            .setContentTitle(applicationContext.getString(R.string.activity_name))
             .setContentText(message)
+            .let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    it.setAllowSystemGeneratedContextualActions(true)
+                } else {
+                    it
+                }
+            }
             .setContentIntent(mainActivityPendingIntent)
             .setAutoCancel(true)
+            .setColorized(true)
+            .setColor(0xff6A00)
+            .setCategory(Notification.CATEGORY_EVENT)
+            .setShowWhen(true)
             .build()
     }
 
 
     @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
-    private fun pushNotification(notification: Notification) {
+    private fun pushNotification(id: Int, notification: Notification) {
         val notificationManager = NotificationManagerCompat.from(applicationContext)
-        notificationManager.notify(0, notification)
+        notificationManager.notify(id, notification)
     }
 }
