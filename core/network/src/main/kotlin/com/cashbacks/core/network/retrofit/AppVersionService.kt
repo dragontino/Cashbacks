2ebe6cb9f079abfe2ef74aaacb7bde7c17760d70
name1980 <petrovsd2002@gmail.com>
2025-11-26 18:00:25 +0300

    Update AppVersionService creation, remove AppVersionRetrofit class

--- core/network/src/main/kotlin/com/cashbacks/core/network/retrofit/AppVersionService.kt
+++ core/network/src/main/kotlin/com/cashbacks/core/network/retrofit/AppVersionService.kt
@@ -1,9 +1,27 @@
 package com.cashbacks.core.network.retrofit
 
+import com.cashbacks.core.network.BuildConfig
 import com.cashbacks.core.network.model.AppReleaseDto
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import retrofit2.http.GET
+
+private const val BASE_URL = BuildConfig.APP_REPOS_URL
 
 internal interface AppVersionService {
     @GET("releases/latest")
     suspend fun getLatestRelease(): AppReleaseDto
+}
+
+internal fun AppVersionService(): AppVersionService {
+    val mediaType = "application/json; charset=UTF8".toMediaType()
+    val json = Json { ignoreUnknownKeys = true }
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(json.asConverterFactory(mediaType))
+        .build()
+
+    return retrofit.create(AppVersionService::class.java)
 }
