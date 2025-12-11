436ba6fc16d84b9e3603c352715a4693940541b2
name1980 <petrovsd2002@gmail.com>
2025-12-11 16:25:56 +0300

    Added implementations for usesCases and LoginRepository in LoginComponentModule

--- components/login/src/main/kotlin/com/cashbacks/components/login/di/LoginComponentModule.kt
+++ components/login/src/main/kotlin/com/cashbacks/components/login/di/LoginComponentModule.kt
@@ -2,6 +2,10 @@ package com.cashbacks.components.login.di
 
 import com.cashbacks.components.login.data.repos.LoginRepositoryImpl
 import com.cashbacks.components.login.domain.repos.LoginRepository
+import com.cashbacks.components.login.domain.usecase.CheckIsAlreadySignedUpUseCase
+import com.cashbacks.components.login.domain.usecase.CheckIsAlreadySignedUpUseCaseImpl
+import com.cashbacks.components.login.domain.usecase.SignInUseCase
+import com.cashbacks.components.login.domain.usecase.SignInUseCaseImpl
 import com.cashbacks.components.login.domain.usecase.SignUpUseCase
 import com.cashbacks.components.login.domain.usecase.SignUpUseCaseImpl
 import kotlinx.coroutines.Dispatchers
@@ -9,10 +13,32 @@ import org.koin.dsl.module
 
 val LoginComponentModule = module {
     single<SignUpUseCase> {
-        SignUpUseCaseImpl(repository = get(), dispatcher = Dispatchers.IO)
+        SignUpUseCaseImpl(
+            repository = get(),
+            dispatcher = Dispatchers.IO,
+            context = get()
+        )
     }
 
+
+    single<SignInUseCase> {
+        SignInUseCaseImpl(
+            repository = get(),
+            dispatcher = Dispatchers.IO,
+            context = get()
+        )
+    }
+
+
+    single<CheckIsAlreadySignedUpUseCase> {
+        CheckIsAlreadySignedUpUseCaseImpl(
+            repository = get(),
+            dispatcher = Dispatchers.IO
+        )
+    }
+
+
     single<LoginRepository> {
-        LoginRepositoryImpl()
+        LoginRepositoryImpl(get(), get())
     }
 }
